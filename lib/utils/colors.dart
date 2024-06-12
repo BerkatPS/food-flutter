93a5b1d3be6e2b15f22045d3663e57779259f926
Berkat Saragih <berkat.sragih@gmail.com>
2024-06-12 23:41:04 +0700

    First Commit

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors {
+  static final Color textColor = const Color(0xFFccc7c5);
+  static final Color mainColor = const Color(0xFFe53935); // Warna merah utama
+  static final Color iconColor1 = const Color(0xFFff8a80); // Merah muda untuk ikon
+  static final Color iconColor2 = const Color(0xFFff5252); // Merah terang untuk ikon
+  static final Color paraColor = const Color(0xFF8f837f);
+  static final Color buttonBgColor = const Color(0xFFffebee); // Latar belakang tombol dengan warna merah muda pucat
+  static final Color signColor = const Color(0xFFef9a9a); // Warna merah muda untuk tanda
+  static final Color titleColor = const Color(0xFFb71c1c); // Warna merah tua untuk judul
+  static final Color mainBlackColor = const Color(0xFF332d2b);
+  static final Color yellowColor = const Color(0xFFffd379); // Warna kuning tetap sama
+}
